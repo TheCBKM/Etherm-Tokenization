67c9b2a604a487e0e689e0a00325687b40cf061f
Rajaram Joshi <38382861+TheCBKM@users.noreply.github.com>
2020-03-15 20:07:51 +0530

    Add files via upload

---
+++ client/build/precache-manifest.29c09dd22218fe3e905f0dfa1e075ee7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf021ac7916734dd04252205b98f324",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00d6f4e820105a1b3ec5",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5bc12bd4c660b5bfe98f",
+    "url": "/static/js/2.2d979d2b.chunk.js"
+  },
+  {
+    "revision": "00d6f4e820105a1b3ec5",
+    "url": "/static/js/main.f9bc0ef9.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
